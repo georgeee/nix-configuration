7b2ddb8acb14892edb72443ef6bc11d11a2140d6
georgeee <george.agapov@gmail.com>
2019-01-26 19:41:12 +0300

    Updated configuration

--- home/.xmonad/recompile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-p='haskellPackages.ghcWithPackages (pkgs: with pkgs; [xmonad xmonad-contrib taffybar])'
-
-nix-shell -p "$p" --run "xmonad --recompile"
-nix-shell -p "$p" --run taffybar
