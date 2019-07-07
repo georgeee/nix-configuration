ec71fd16ad6f1870c1fb21d04566baffa5ac49ca
georgeee <george.agapov@gmail.com>
2019-07-07 14:52:39 +0300

    script

--- sync.sh
+++ sync.sh
@@ -4,4 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
-rsync -r /etc/nixos/* global && git add global && git commit -m "Updated configuration"
+mkdir -p home/.xmonad home/.local/share home/.kde/share/apps home/.config
+
+  rsync -r /etc/nixos/* global && \
+  rsync -r ~/.xmonad/{xmonad.hs,scripts} home/.xmonad && \
+  cp ~/{.gitconfig,.bg.jpg,.emacs,.tmux.conf} home && \
+  rsync -r ~/.local/share/konsole home/.local/share && \
+  rsync -r ~/.kde/share/apps/konsole home/.kde/share/apps && \
+  rsync -r ~/.config/taffybar home/.config && \
+  git add home global && \
+  git commit -m "Updated configuration" ; git push
